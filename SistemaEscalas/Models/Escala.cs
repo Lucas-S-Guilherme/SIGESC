51b8666ece558b42ddf11ced4223bc5748d8df49
Lucas <lucassguilherme159@gmail.com>
2024-12-18 00:02:58 -0400

    desenvolvendo

--- SistemaEscalas/Models/Escala.cs
+++ SistemaEscalas/Models/Escala.cs
@@ -1,11 +1,20 @@
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SistemaEscalas.Models
 {
+
+    [Table("escala")]
     public class Escala
     {
         public int id {get; set;}
-        public string? nome {get; set;}
+        public string? tipo {get; set;}
+        public DateTime dataInicio {get; set;}
+        public DateTime dataFinal {get; set;}
+        public string? descricao {get; set;}
+        public string? local_trabalho {get; set;}
+        public string? regime_trabalho {get;set;}
+        
 
-        public DateTime turno {get; set;}
     }
 }
